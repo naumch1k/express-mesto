9966e82047341dbf85c8969b2318aa664d110096
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-07-25 15:25:17 -0500

    feat: create user controllers and routes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
