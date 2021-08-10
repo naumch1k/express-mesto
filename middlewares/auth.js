cc8fceb7590b7c6bb16b4d0f2a94b49eceb11d11
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-08-10 09:33:35 -0500

    hotfix: fix auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const StatusCodes = require('../utils/status-codes');
+const { NotFoundError } = require('../errors/index');
 const StatusMessages = require('../utils/status-messages');
 
 // const { JWT_SECRET } = process.env;
@@ -8,7 +8,7 @@ const StatusMessages = require('../utils/status-messages');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
-    return res.status(StatusCodes.UNAUTHORIZED).send({ message: StatusMessages.UNAUTHORIZED });
+    throw new NotFoundError(StatusMessages.UNAUTHORIZED);
   }
 
   const token = req.cookies.jwt;
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res.status(StatusCodes.UNAUTHORIZED).send({ message: StatusMessages.UNAUTHORIZED });
+    throw new NotFoundError(StatusMessages.UNAUTHORIZED);
   }
 
   req.user = payload;
